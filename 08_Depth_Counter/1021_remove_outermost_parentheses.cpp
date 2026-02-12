7e7d7494ab3ea8f401876a5a30c7e0d56e585bc0
Smit0401 <smit040106@gmail.com>
2026-02-12 23:32:08 +0530

    Create 1021_remove_outermost_parentheses.cpp

---
+++ 08_Depth_Counter/1021_remove_outermost_parentheses.cpp
@@ -0,0 +1,24 @@
+// 1021. Remove Outermost Parentheses
+// Difficulty: Easy
+// Pattern: Depth Counter
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int depth = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                if (depth > 0) ans += c; 
+                depth++;
+            } else { // ')'
+                depth--;
+                if (depth > 0) ans += c; 
+            }
+        }
+        return ans;
+    }
+};
